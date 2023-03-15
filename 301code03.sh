7958c26d053cbca0e8a8d6269d149096c0360ba9
Nickolaus Alderete <122177990+Nkalderete@users.noreply.github.com>
2023-03-15 18:26:44 -0500

    Update 301code03.sh

---
+++ 301code03.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Nick A- worked with Sierra, Justin H and Geneva
+
+
+# Prompt user for doc
+read -p "Directory path you would like to edit permissions?" Directory
+
+# Prompt user for command 
+read -p "Type command to change permissions?" Code
+
+# Check file permissions
+ls -l "$Directory" 
+
+# Change permissions
+chmod -R "$Code" "$Directory"
+
+echo "Permissions for directory and its contents updated successfully" 
