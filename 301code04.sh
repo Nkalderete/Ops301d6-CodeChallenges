95c2aa3436b285888c5577da113ddd109eb4eb8c
nkalderete <nk.alderete@gmail.com>
2023-03-16 23:27:33 +0000

    301 code challenge, class 04

---
+++ 301code04.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+while true; do
+    clear
+    echo "Please select an option:"
+    echo "1. Hello world"
+    echo "2. Ping self"
+    echo "3. IP info"
+    echo "4. Exit"
+
+    read -p "Enter your choice: " choice
+
+    case $choice in
+        1)
+            echo "Hello world!"
+            read -p "Press enter to continue"
+            ;;
+        2)
+            ping -c 4 127.0.0.1
+            read -p "Press enter to continue"
+            ;;
+        3)
+            ifconfig
+            read -p "Press enter to continue"
+            ;;
+        4)
+            exit 0
+            ;;
+        *)
+            echo "Invalid choice. Press enter to try again."
+            read
+            ;;
+    esac
+done
