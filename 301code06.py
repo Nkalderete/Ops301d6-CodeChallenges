c4ff5301d0a536da34bd1caeba0509f9581c9bb5
nkalderete <nk.alderete@gmail.com>
2023-03-20 22:44:35 +0000

    301 code challenge, class 06

---
+++ 301code06.py
@@ -0,0 +1,24 @@
+
+
+# print current directory
+print ("Current working directory")
+
+
+# import os module
+import os 
+
+# var
+sam= os.system("ls")
+nikoda= os.system("whoami")
+zay= os.system("lshw -short")
+
+
+# execute
+print (sam, nikoda, zay)
+
+
+# print 3x
+print ("this is my final print")
+
+
+
