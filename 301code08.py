520f9cc98c710f0d27e744568cd345fef291c2f3
nkalderete <nk.alderete@gmail.com>
2023-03-22 22:38:33 +0000

    301 code challenge, class 08

---
+++ 301code08.py
@@ -0,0 +1,23 @@
+# main
+
+
+# Define the list
+my_list = ["apples", "bananas", "oranges", "pears", "grapes", "kiwis", "peaches", "plums", "cherries", "mangoes"]
+
+
+#print fourth element of list
+print("The fourth element is...", my_list[3])
+
+
+#print the sixth through tenth element
+print("Elements 6 through 10 are...", my_list[5:10])
+
+
+# Change the 7th element to onion
+my_list[6]=("onions")
+
+# Confirm change
+print("The new list with the 7th element changed to onion is...", my_list)
+
+
+# end
